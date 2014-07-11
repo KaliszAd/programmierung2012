b4c7a0a4d2591bddc38ce6b7d03e9d0264deaa25
Adam Kalisz <adam_kalisz@wh2.tu-dresden.de>
2014-07-11 19:53:24 +0200

    Changes to Aufgabe 12.10

--- aufgabe12_10.hs
+++ aufgabe12_10.hs
@@ -24,3 +24,46 @@ countB (A n ta tb)	= (countB ta) + (countB' tb)
 countB' :: TB -> Int
 countB' NilB		= 0
 countB' (B n tb ta)	= 1 + (countB' tb) + (countB ta)
+
+{-
+
+The solution in the book is more efficient
+
+trans :: TA -> TA
+trans t = atrans t 0
+
+atrans :: TA -> Int -> TA
+atrans NilA _ 			= NilA
+atrans (A _ ta tb) n 	= (A n (atrans ta n) (btrans tb n))
+
+btrans :: TB -> Int -> TB
+btrans NilB _ 			= NilB
+btrans (B _ tb ta) n	= (B (n + 1) (btrans tb (n + 1)) (atrans ta (n + 1)))
+-- Or without the change of the value stored on the B-Branch
+btrans (B val tb ta) n	= (B val (btrans tb (n + 1)) (atrans ta (n + 1)))
+
+-}
+
+-- c)
+-- These functions save all Integers in the Tree in a list
+
+list_naive :: TA -> [Int]
+list_naive NilA			= []
+list_naive (A n ta tb)	= (list ta) ++ [n] ++ (list' tb)
+
+list_naive' :: TB -> [Int]
+list_naive' NilB		= []
+list_naive' (B n tb ta)	= (list' tb) ++ [n] ++ (list ta)
+
+-- This is the actual solution, it saves only the values stored in a 
+-- trees
+
+list :: TA -> [Int]
+list NilA			= []
+list (A n ta tb)	= [n] ++ (list ta) ++ (list' tb)
+-- I think, here it should be (list ta) ++ [n] ++ (list' tb)
+-- but that isn't in the solution
+
+list' :: TB -> [Int]
+list' NilB			= []
+list' (B n tb ta)	= (list' tb) ++ (list ta)
