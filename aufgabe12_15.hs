f766c53fc12cd32cb6b6fc2a3803e9c00bb6c0b5
Adam Kalisz <adam_kalisz@wh2.tu-dresden.de>
2014-07-12 21:53:22 +0200

    Corrected Aufgabe 12.15, more examples for testing and better understanding

--- aufgabe12_15.hs
+++ aufgabe12_15.hs
@@ -10,24 +10,29 @@ trans ((x,y):xys)
 	| otherwise 	= [(x + y)] ++ (trans xys)
 
 -- b)
--- This is not done!
+-- This one is a bit tricky because of the last case, when there is no x 
+-- higher than n. That is the 'liste n []' case.
+-- `liste (a) [0,1,2,3,4,5,6]` for a, try lower number, than contained 
+-- in the list, number contained in the list and number higher than 
+-- contained in the list
 
 liste :: Int -> [Int] -> [Int]
-liste n []		= []
+liste n []		= [n]
 liste n (x:xs)
-	| n < x 	= [n] ++ xs
-	| n == x	= liste n xs
-	| n > x		= [x] ++ liste n xs
+	| n < x 	= [n] ++ [x] ++ xs
+	| n == x	= []  ++ xs
+	| n > x		= [x] ++ (liste n xs) 
 
 -- c)
+-- `transtree (Node (Leaf 1) (Node (Leaf 2) (Leaf 3) False) False)`
 
 data Tree a = Node (Tree a) (Tree a) Bool | Leaf a deriving Show
 
-transtree :: Tree Bool -> Tree Bool
+transtree :: Tree a -> Tree a
 transtree (Leaf a)			= Leaf a
 transtree (Node t1 t2 b)
-	| (number_leafs t1) == (number_leafs t2)	= Node t1 t2 True
-	| otherwise									= Node t1 t2 False
+	| (number_leafs t1) == (number_leafs t2)	= Node (transtree t1) (transtree t2) True
+	| otherwise									= Node (transtree t1) (transtree t2) False
 
 number_leafs :: Tree a -> Int
 number_leafs (Leaf a)		= 1
